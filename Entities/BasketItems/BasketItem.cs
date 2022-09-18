70d8cb318d5c50640412110b9ae213869510a035
halilkorukmez <halilkorukmez@clebracode.com>
2022-09-18 13:59:42 +0300

    fun

--- Entities/BasketItems/BasketItem.cs
+++ Entities/BasketItems/BasketItem.cs
@@ -6,6 +6,7 @@ public class BasketItem
 {
     public Guid ProductId { get; set; }
     public bool IsActive { get; set; }
+    
     public DateTime CreatedDate { get; set; } = DateTime.Now;
     public string Name { get; set; }
     public decimal Price { get; set; }
